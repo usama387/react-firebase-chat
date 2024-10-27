147a447b98ddb67d10a98e0dfbd0e25a92a8f3a2
usama387 <128306049+usama387@users.noreply.github.com>
2024-10-27 22:15:20 +0500

    Created 3 components added them in a horizontal flex box and divided the space and imported them in the root file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import List from "./components/list/List";
+import Chat from "./components/chat/Chat";
+import Detail from "./components/detail/Detail";
 
 const App = () => {
-  return <div className="container">Home</div>;
+  return (
+    <div className="container">
+      <List />
+      <Chat />
+      <Detail />
+    </div>
+  );
 };
 
 export default App;
