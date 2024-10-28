c164cff8eff0cdd93ef417c37b1dc203da352402
usama387 <128306049+usama387@users.noreply.github.com>
2024-10-28 17:54:45 +0500

    Login Page initial design integrated along with image upload logic in SingUp section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import List from "./components/list/List";
 import Chat from "./components/chat/Chat";
 import Detail from "./components/detail/Detail";
+import Login from "./components/login//Login";
 
 const App = () => {
+  // dummy user
+  const user = false;
+
   return (
     <div className="container">
-      <List />
-      <Chat />
-      <Detail />
+      {user ? (
+        <>
+          <List />
+          <Chat />
+          <Detail />
+        </>
+      ) : (
+        <Login />
+      )}
     </div>
   );
 };
