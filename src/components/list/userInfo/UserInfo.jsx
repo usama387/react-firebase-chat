620cbb5c8b0f2e56469372072858f5ee5177a680
usama387 <128306049+usama387@users.noreply.github.com>
2024-10-29 19:33:03 +0500

    UserInfo component made dynamic with currentUser from useUserStore

--- src/components/list/userInfo/UserInfo.jsx
+++ src/components/list/userInfo/UserInfo.jsx
@@ -1,17 +1,23 @@
-import "./userInfo.css"
+import "./userInfo.css";
+import { useUserStore } from "../../../lib/userStore";
 
 const UserInfo = () => {
-  return <div className="userInfo">
-    <div className="user">
-      <img src="/avatar.png"/>
-      <h2>Usama Razaaq</h2>
+  // getting my user from zustand config in userStore.js
+  const { currentUser } = useUserStore();
+
+  return (
+    <div className="userInfo">
+      <div className="user">
+        <img src={currentUser.avatar || "/avatar.png"} />
+        <h2>{currentUser.username}</h2>
+      </div>
+      <div className="icons">
+        <img src="/more.png" />
+        <img src="/video.png" />
+        <img src="/edit.png" />
+      </div>
     </div>
-    <div className="icons">
-      <img src="/more.png"/>
-      <img src="/video.png"/>
-      <img src="/edit.png"/>
-    </div>
-  </div>;
+  );
 };
 
 export default UserInfo;
