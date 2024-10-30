6abf90dfbb60a24dfeeb713b97cab760ace28a5a
usama387 <128306049+usama387@users.noreply.github.com>
2024-10-30 10:46:48 +0500

    Logic added in the AddUser component to find user using username

--- src/components/list/chatList/addUser/AddUser.jsx
+++ src/components/list/chatList/addUser/AddUser.jsx
@@ -1,19 +1,51 @@
+import { toast } from "react-toastify";
+import { database } from "../../../../lib/firebase";
 import "./addUser.css";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { useState } from "react";
 
 const AddUser = () => {
+  // managing user state to add user
+  const [user, setUser] = useState(null);
+
+  // finds the user in the db passing username in query
+  const handleSearch = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const username = formData.get("username");
+
+    try {
+      const userRef = collection(database, "users");
+
+      const q = query(userRef, where("username", "==", username));
+
+      const querySnapShot = await getDocs(q);
+
+      if (!querySnapShot.empty) {
+        setUser(querySnapShot.docs[0].data());
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
   return (
     <div className="addUser">
-      <form>
+      <form onSubmit={handleSearch}>
         <input type="text" placeholder="username" name="username" />
         <button>Search</button>
       </form>
-      <div className="user">
-        <div className="detail">
-            <img src="./avatar.png" alt="" />
-            <span>Usama Razaaq</span>
+      {user && (
+        <div className="user">
+          <div className="detail">
+            <img src={user.avatar || "./avatar.png"} alt="" />
+            <span>{user.username}</span>
+          </div>
+          <button>Add User</button>
         </div>
-        <button>Add User</button>
-      </div>
+      )}
     </div>
   );
 };
