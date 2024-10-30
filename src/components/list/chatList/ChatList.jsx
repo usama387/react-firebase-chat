195bb425bb0cdf55b6a4f21cfd59783605c3f8a2
usama387 <128306049+usama387@users.noreply.github.com>
2024-10-30 10:25:48 +0500

    Logic added in ChatList to fetch chats and then take out receiver id from the response and take user from there and sort them and pass it to setChats state to display it in the list

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,10 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AddUser from "./addUser/AddUser";
 import "./chatList.css";
+import { useUserStore } from "../../../lib/userStore";
+import { doc, getDoc, onSnapshot } from "firebase/firestore";
+import { database } from "../../../lib/firebase";
 
 const ChatList = () => {
   // to manage add user icon state
   const [addMode, setAddMode] = useState(false);
+
+  // after fetching chats with useEffect storing it in useState
+  const [chats, setChats] = useState([]);
+
+  // getting currentUser
+  const { currentUser } = useUserStore();
+
+  useEffect(() => {
+    // fetching chats by passing id and userChats key from db
+    const unSub = onSnapshot(
+      doc(database, "userChats", currentUser.id),
+      async (response) => {
+        
+        const items = response.data().chats();
+
+        const promises = items.map(async (item) => {
+          const userDocRef = doc(database, "users", item.receiverId);
+          const userDocSnap = await getDoc(userDocRef);
+
+          const user = userDocSnap.data();
+
+          return { ...item, user };
+        });
+
+        const chatData = await Promise.all(promises);
+        // updating state with setChats
+        setChats(chatData.sort((a, b) => b.updatedAt - a.updatedAt));
+      }
+    );
+    return () => {
+      unSub();
+    };
+  }, [currentUser.id]);
 
   return (
     <div className="chatList">
@@ -22,28 +58,17 @@ const ChatList = () => {
         />
       </div>
 
-      {/* chat items logic */}
-      <div className="item">
-        <img src="./avatar.png" alt="" />
-        <div className="texts">
-          <span>Usama Razaaq</span>
-          <p>Hello</p>
+      {/* chat content logic */}
+      {chats.map((chat) => (
+        <div className="item" key={chat.chatId}>
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Usama Razaaq</span>
+            <p>{chat.lastMessage}</p>
+          </div>
         </div>
-      </div>
-      <div className="item">
-        <img src="./avatar.png" alt="" />
-        <div className="texts">
-          <span>Usama Razaaq</span>
-          <p>Hello</p>
-        </div>
-      </div>
-      <div className="item">
-        <img src="./avatar.png" alt="" />
-        <div className="texts">
-          <span>Usama Razaaq</span>
-          <p>Hello</p>
-        </div>
-      </div>
+      ))}
+
       {/* when addMode is true the addUser component is rendered */}
       {addMode && <AddUser />}
     </div>
